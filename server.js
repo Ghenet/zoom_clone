2fdcfd2a2401497a9b1b6bdbefb2258f00b83eb6
Ghenet Andemiachael <ghenetandemiachael@Ghenets-MacBook-Pro.local>
2020-08-05 11:17:43 -0700

    sets the view engine to ejs

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const app = express();
 
 const server = require('http').Server(app);
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-    res.status(200).send("Hello World");
+    res.render('room');
 })
 
 server.listen(3030);
